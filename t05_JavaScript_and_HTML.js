e5ef0683d621b5b1ed76eecda119365648ab5cb9
VlctorLuo <24176vl@hvhs.school.nz>
2026-04-30 13:29:40 +1200

    t04 debugging

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,28 @@
-/* Paste your code from task 3, Maths here */
+/****************************
+Name of task: Introduction
+****************************/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput")
+console.log("Running task 05_JavaScript_and_HTML")
+// Variables
+let username = "Victor"
+let age = 15
+let money = 9999999999
+let year = 2026
+var answer
+/*****************
+Main Code
+*****************/
+console.log("Hi "+ username);
+console.log("As of " + year + " you are " + age + " years old");
+console.log("You were born in", + year - age);
+console.log("In 10 years will will be", age + 10 ,"years old");
+console.log("You have " + money + " dollars");
+answer = money/2
+console.log("If you spend half of your money, now you have", + money / 2, "dollars");
+console.log("Then you get $3, now you have", + answer+3);
+
+OUTPUT.innerHTML = "<h1>Hi " + username + "</h1>";
+/*****************
+Functions
+*****************/
+
